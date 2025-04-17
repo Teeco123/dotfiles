653fc83834e1df35a6033572d94d9dba3e1487c2
Kacper Gajko <kacper.gajko1@icloud.com>
2025-04-17 14:12:43 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,34 +6,77 @@ local config = {}
 config.color_scheme = "ChallengerDeep"
 
 wezterm.on("gui-startup", function(cmd)
+	--
 	-- Starting default workspace
+	--
 	local defaultTab, defaultPane, defaultWindow = mux.spawn_window({
 		workspace = "Default",
 		cwd = wezterm.home_dir,
 	})
 	defaultWindow:gui_window():maximize()
 
-	-- AI Calendar workspace
-	local tab1, pane1, window = mux.spawn_window({
-		workspace = "Aicalendar",
-		cwd = "/Users/kacper/Work/aicalendar/aicalendar",
+	--
+	-- Water renderer workspace
+	--
+	local tabWR1, paneWR1, windowWR = mux.spawn_window({
+		workspace = "Water renderer",
+		cwd = "/Users/kacper/Developer/water-renderer/",
 	})
-	tab1:set_title("AICalendar")
+	tabWR1:set_title("nvim")
+	tabWR1:activate()
+	paneWR1:send_text("nvim\n")
 
-	local tab2, pane2 = window:spawn_tab({
-		cwd = "/Users/kacper/Work/aicalendar/app-web",
+	local tabWR2, paneWR2 = windowWR:spawn_tab({
+		cwd = "/Users/kacper/Developer/water-renderer/",
 	})
-	tab2:set_title("app-web")
+	tabWR2:set_title("build")
 
-	local tab3, pane3 = window:spawn_tab({
-		cwd = "/Users/kacper/Work/aicalendar/api-user-service",
+	--
+	-- Multiplayer game workspace
+	--
+	local tabMG1, paneMG1, windowMG = mux.spawn_window({
+		workspace = "Mutliplayer game",
+		cwd = "/Users/kacper/Developer/multiplayer-game/",
 	})
-	tab3:set_title("user-service")
+	tabMG1:set_title("nvim")
+	tabMG1:activate()
+	paneMG1:send_text("nvim\n")
 
-	local tab4, pane4 = window:spawn_tab({
-		cwd = "/Users/kacper/Work/aicalendar/api-event-service",
+	local tabMG2, paneMG2 = windowMG:spawn_tab({
+		cwd = "/Users/kacper/Developer/multiplayer-game/",
 	})
-	tab3:set_title("event-service")
+	tabMG2:set_title("server")
+
+	local tabMG3, paneMG3 = windowMG:spawn_tab({
+		cwd = "/Users/kacper/Developer/multiplayer-game/",
+	})
+	tabMG3:set_title("client1")
+
+	local tabMG4, paneMG4 = windowMG:spawn_tab({
+		cwd = "/Users/kacper/Developer/multiplayer-game/",
+	})
+	tabMG4:set_title("client2")
+
+	--
+	-- ADHD app workspace
+	--
+	local tabADHD1, paneADHD1, windowADHD = mux.spawn_window({
+		workspace = "ADHD app",
+		cwd = "/Users/kacper/Developer/adhd-app/",
+	})
+	tabADHD1:set_title("nvim")
+	tabADHD1:activate()
+	paneADHD1:send_text("nvim\n")
+
+	local tabADHD2, paneADHD2 = windowADHD:spawn_tab({
+		cwd = "/Users/kacper/Developer/adhd-app/",
+	})
+	tabADHD2:set_title("git")
+
+	local tabADHD3, paneADH3 = windowADHD:spawn_tab({
+		cwd = "/Users/kacper/Developer/adhd-app/",
+	})
+	tabADHD3:set_title("server")
 
 	-- Set default workspace
 	mux.set_active_workspace("Default")
