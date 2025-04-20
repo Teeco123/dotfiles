0075d09dd7c5cdebec75da2d7349935065bb7b7f
Kacper Gajko <kacper.gajko1@icloud.com>
2025-04-20 15:29:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,9 +27,59 @@ brew install --cask wezterm
 
 # macOS Settings
 echo "Changing macOS defaults..."
+
+## Dock
 defaults write com.apple.dock autohide-delay -float 0.0
 defaults write com.apple.dock autohide-time-modifier -float 0.6;
 defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock minimize-to-application -bool true
+defaults write com.apple.dock mineffect -string "scale"
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock "show-recents" -bool false
+
+## Keyboard 
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write -globalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write -globalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write -globalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+## Touchbar
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+## Finder
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Kill affected apps
+for app in "Dock" "Finder"; do
+  killall "${app}" > /dev/null 2>&1
+done
+
+# Done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
